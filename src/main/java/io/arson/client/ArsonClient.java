2b14f39031d6beb15ed0e9998c822ebfa4240361
peli1gamer <leevilarkkonen@gmail.com>
2026-09-10 22:43:32 +0300

    Add Arson client entrypoint and Right Shift menu binding

---
+++ src/main/java/io/arson/client/ArsonClient.java
@@ -0,0 +1,66 @@
+package io.arson.client;
+
+import io.arson.client.module.ModuleManager;
+import io.arson.client.ui.ArsonScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import com.mojang.blaze3d.platform.InputConstants;
+import org.lwjgl.glfw.GLFW;
+
+public final class ArsonClient implements ClientModInitializer {
+    public static final String MOD_ID = "arson";
+
+    private static ArsonClient instance;
+    private ModuleManager moduleManager;
+    private KeyMapping openMenuKey;
+
+    public static ArsonClient getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onInitializeClient() {
+        instance = this;
+        moduleManager = new ModuleManager();
+        moduleManager.registerDefaults();
+
+        KeyMapping.Category category = KeyMapping.Category.register(
+                net.minecraft.resources.Identifier.fromNamespaceAndPath(MOD_ID, "main")
+        );
+
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.arson.open_menu",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                category
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openMenuKey.consumeClick()) {
+                Screen current = client.screen;
+                if (current instanceof ArsonScreen) {
+                    client.setScreen(null);
+                } else {
+                    client.setScreen(new ArsonScreen(current));
+                }
+            }
+
+            moduleManager.tick(client);
+        });
+
+        Component startup = Component.literal("Arson V3 initialized");
+        Minecraft client = Minecraft.getInstance();
+        if (client.player != null) {
+            client.player.displayClientMessage(startup, true);
+        }
+    }
+
+    public ModuleManager modules() {
+        return moduleManager;
+    }
+}
