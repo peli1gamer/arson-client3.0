acdcab9c32d77ee57771ea2ce6b767aaf403a4d6
peli1gamer <leevilarkkonen@gmail.com>
2026-09-11 11:45:01 +0300

    Add storage render categories

---
+++ src/main/java/com/arson/client/render/StorageType.java
@@ -0,0 +1,10 @@
+package com.arson.client.render;
+
+/** Storage categories used by the configurable storage overlay. */
+public enum StorageType {
+    CHEST,
+    BARREL,
+    SHULKER,
+    ENDER_CHEST,
+    OTHER
+}
