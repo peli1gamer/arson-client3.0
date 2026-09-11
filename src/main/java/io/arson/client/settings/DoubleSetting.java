0bc6222b1eff59b3e94d4f94caa647efa13dca33
peli1gamer <leevilarkkonen@gmail.com>
2026-09-11 10:10:30 +0300

    Add numeric client setting

---
+++ src/main/java/io/arson/client/settings/DoubleSetting.java
@@ -0,0 +1,24 @@
+package io.arson.client.settings;
+
+public final class DoubleSetting extends Setting<Double> {
+    private final double min;
+    private final double max;
+    private final double step;
+
+    public DoubleSetting(String id, String name, double defaultValue, double min, double max, double step) {
+        super(id, name, defaultValue);
+        if (min > max || step <= 0) throw new IllegalArgumentException("Invalid numeric setting range");
+        this.min = min;
+        this.max = max;
+        this.step = step;
+    }
+
+    @Override
+    public void set(Double value) {
+        super.set(Math.max(min, Math.min(max, value)));
+    }
+
+    public double min() { return min; }
+    public double max() { return max; }
+    public double step() { return step; }
+}
