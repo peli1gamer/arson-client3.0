7024bcae88d54d049ef50ddfd167cce6988da8b1
peli1gamer <leevilarkkonen@gmail.com>
2026-09-14 19:17:27 +0300

    Add configurable entity ESP health bar colors

--- src/main/java/io/arson/client/module/EntityESPModule.java
+++ src/main/java/io/arson/client/module/EntityESPModule.java
@@ -13,10 +13,13 @@ public final class EntityESPModule extends VisualModule implements EntityScanCon
     private final BooleanSetting animals = setting(new BooleanSetting("animals", "Animals", false));
     private final BooleanSetting items = setting(new BooleanSetting("items", "Dropped Items", false));
     private final BooleanSetting showHealth = setting(new BooleanSetting("health", "Health", false));
+    private final BooleanSetting healthBackground = setting(new BooleanSetting("health-background", "Health Background", true));
     private final ColorSetting playerColor = setting(new ColorSetting("player-color", "Player Color", 0xD655AAFF));
     private final ColorSetting mobColor = setting(new ColorSetting("mob-color", "Mob Color", 0xD6FF5555));
     private final ColorSetting animalColor = setting(new ColorSetting("animal-color", "Animal Color", 0xD655FF78));
     private final ColorSetting itemColor = setting(new ColorSetting("item-color", "Item Color", 0xD6FFDC46));
+    private final ColorSetting healthColor = setting(new ColorSetting("health-color", "Health Color", 0xF6FF5555));
+    private final ColorSetting healthBackgroundColor = setting(new ColorSetting("health-background-color", "Health Background Color", 0xC6202020));
     private final DoubleSetting range = setting(new DoubleSetting("range", "Range", 64.0, 8.0, 128.0, 4.0));
     private final DoubleSetting scanInterval = setting(new DoubleSetting("scan-interval", "Scan Interval", 2.0, 1.0, 10.0, 1.0));
 
@@ -29,12 +32,15 @@ public final class EntityESPModule extends VisualModule implements EntityScanCon
     public boolean showAnimals() { return animals.enabled(); }
     public boolean showItems() { return items.enabled(); }
     public boolean showHealth() { return showHealth.enabled(); }
+    public boolean showHealthBackground() { return healthBackground.enabled(); }
     public boolean fill() { return super.filled(); }
     public boolean outline() { return super.outline(); }
     public int playerColor() { return playerColor.get(); }
     public int mobColor() { return mobColor.get(); }
     public int animalColor() { return animalColor.get(); }
     public int itemColor() { return itemColor.get(); }
+    public int healthColor() { return healthColor.get(); }
+    public int healthBackgroundColor() { return healthBackgroundColor.get(); }
     public double fillAlpha() { return super.fillAlpha(); }
     public double outlineAlpha() { return super.outlineAlpha(); }
     public double lineWidth() { return super.lineWidth(); }
