a2487b226f0df52250483d356043a43ff1ec4590
peli1gamer <leevilarkkonen@gmail.com>
2026-09-15 17:48:38 +0300

    Add setting defaults and reset support

--- src/main/java/io/arson/client/settings/Setting.java
+++ src/main/java/io/arson/client/settings/Setting.java
@@ -3,16 +3,20 @@ package io.arson.client.settings;
 public abstract class Setting<T> {
     private final String id;
     private final String name;
+    private final T defaultValue;
     private T value;
 
     protected Setting(String id, String name, T defaultValue) {
         this.id = id;
         this.name = name;
+        this.defaultValue = defaultValue;
         this.value = defaultValue;
     }
 
     public String id() { return id; }
     public String name() { return name; }
     public T get() { return value; }
+    public T defaultValue() { return defaultValue; }
     public void set(T value) { this.value = value; }
+    public void reset() { this.value = defaultValue; }
 }
