8f96ea8c4b8c7637a1229787a7f0730ecf4fc8cb
peli1gamer <leevilarkkonen@gmail.com>
2026-09-15 17:34:15 +0300

    Add Block ESP target model

---
+++ src/main/java/io/arson/client/render/BlockTarget.java
@@ -0,0 +1,9 @@
+package io.arson.client.render;
+
+import com.arson.client.render.RenderStyle;
+
+/** Cached block target handed from block discovery to the world renderer. */
+public record BlockTarget(double minX, double minY, double minZ,
+                          double maxX, double maxY, double maxZ,
+                          RenderStyle style, double distance) {
+}
