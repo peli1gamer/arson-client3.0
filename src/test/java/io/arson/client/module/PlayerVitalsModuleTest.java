2ca35f2b43f6a84ae84976402cdbe545e7c42b3d
peli1gamer <leevilarkkonen@gmail.com>
2026-09-16 21:59:26 +0300

    Test live player vitals module

---
+++ src/test/java/io/arson/client/module/PlayerVitalsModuleTest.java
@@ -0,0 +1,21 @@
+package io.arson.client.module;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerVitalsModuleTest {
+    @Test
+    void exposesLiveTelemetrySurfaceAndSafeInitialState() {
+        PlayerVitalsModule module = new PlayerVitalsModule();
+        assertEquals("player-vitals", module.id());
+        assertEquals(Module.Category.PLAYER, module.category());
+        assertTrue(module.description().contains("health"));
+        assertEquals(0, module.level());
+        assertEquals(0, module.totalExperience());
+        assertEquals("Air 0/0", module.formattedAir());
+        assertTrue(module.formattedExperience().contains("Level 0"));
+        assertFalse(module.sprinting());
+        assertFalse(module.sneaking());
+    }
+}
