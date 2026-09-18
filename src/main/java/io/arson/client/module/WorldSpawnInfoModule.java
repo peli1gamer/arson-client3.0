1a4b52a1fbe72169e9f5d769d47e0470c06b39cd
peli1gamer <leevilarkkonen@gmail.com>
2026-09-18 19:13:53 +0300

    Use 1.21.11 client spawn accessors

--- src/main/java/io/arson/client/module/WorldSpawnInfoModule.java
+++ src/main/java/io/arson/client/module/WorldSpawnInfoModule.java
@@ -20,10 +20,10 @@ public final class WorldSpawnInfoModule extends Module {
             distance = 0;
             return;
         }
-        var spawn = client.level.getSharedSpawnPos();
-        spawnX = spawn.getX();
-        spawnY = spawn.getY();
-        spawnZ = spawn.getZ();
+        var data = client.level.getLevelData();
+        spawnX = data.getXSpawn();
+        spawnY = data.getYSpawn();
+        spawnZ = data.getZSpawn();
         double dx = client.player.getX() - spawnX;
         double dz = client.player.getZ() - spawnZ;
         distance = Math.sqrt(dx * dx + dz * dz);
